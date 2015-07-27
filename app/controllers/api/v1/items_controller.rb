5e47dd5df95d5fef31e4a498762cccb1d315cee9
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-07-27 10:52:51 -0600

    Create the Item show & Item create API methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::ItemsController < ApplicationController
   def index
     respond_with Item.all
   end
+
+  def show
+    respond_with Item.find(params[:id])
+  end
+
+  def create
+    respond_with Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
 end
